c6383012026eb99df3d6f234ecccc462ea5f5abc
Asha <ashapoulose0@gmail.com>
2022-03-30 13:35:41 +0530

    test operations

---
+++ operators.js
@@ -0,0 +1,34 @@
+// Arithmetic operators
+
+console.log(2 + 3);
+var a=2+3;
+console.log("addition :" + a);
+a ++;
+console.log("addition :" + a);
+var b= 2 * 3;
+console.log(b);
+var c=5/2;
+console.log(c);
+var d =5%2; // modulas -> which gives remainder
+console.log(d);
+
+// Assignment operators
+
+var s=8;
+s+=2; // s=s+2;
+console.log(s);
+var t=8;
+t-=2; // t=t+2;
+console.log(t);
+
+// Comparison operators
+
+var x=2;
+var y=2;
+if(x==y){
+	document.write("sum is : " );
+	document.write(x + y);
+	console.log("x==y");
+}else{
+	console.log("x!=y");
+}
