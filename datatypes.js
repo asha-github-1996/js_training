acac32fdcb795fe6f9188b338e1be6211add77af
Asha <ashapoulose0@gmail.com>
2022-03-30 13:41:37 +0530

    test datatypes

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+var a;
+a=1;										
+console.log(a);
+var b=prompt("enter some value"); 
+// (prompt value will always be string whatever we enter)
+console.log(typeof b);
+// console.log(b + 4);
+// parseInt(b) -> will convert the string value to integer
+console.log(parseInt(b) + 4);
+
+alert("ths is an alert");
