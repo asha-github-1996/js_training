59d76b0fe07875990025659865ffe08fc9276ec5
Asha <ashapoulose0@gmail.com>
2022-04-01 12:10:25 +0530

    code challenge1

---
+++ code_challenge1.js
@@ -0,0 +1,20 @@
+let Marksweight=78;
+let Marksheight=1.69;
+let Johnweight = 92;
+let Johnheight = 1.95;
+
+let marksBmi=Marksweight/Marksheight**2;
+console.log(marksBmi);
+let johnBmi=Johnweight/Johnheight**2;
+
+markhigherbmi = marksBmi > johnBmi;
+console.log(markhigherbmi);
+
+// if(marksBmi > johnBmi){
+//     console.log("Marks BMI is greater than John");
+// }
+if (marksBmi > johnBmi) {
+    console.log("Mark's BMI (" + marksBmi + ") is higher than John's (" + johnBmi + ")!")
+  } else {
+    console.log(`John's BMI (${johnBmi}) is higher than Marks's (${marksBmi})!`)
+  }
