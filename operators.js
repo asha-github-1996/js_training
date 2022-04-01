553923350bdca7306535463154a16003bfa65444
Asha <ashapoulose0@gmail.com>
2022-04-01 10:56:16 +0530

    test operators

--- operators.js
+++ operators.js
@@ -1,30 +1,34 @@
 // Arithmetic operators 
 
 console.log(2 + 3);
-var a=2+3;
+let a=2+3;
 console.log("addition :" + a);
 a ++;
 console.log("addition :" + a);
-var b= 2 * 3;
+let b= 2 * 3;
 console.log(b);
-var c=5/2;
+let c=5/2;
 console.log(c);
-var d =5%2; // modulas -> which gives remainder
+let d =5%2; // modulas -> which gives remainder
 console.log(d);
+
+const ageJonas = 2037 - 1991;
+const ageRiya = 2027 - 1991;
+console.log("Jonas age : " + ageJonas ,"Riya age : " + ageRiya);
 
 // Assignment operators
 
-var s=10;
+let s=10;
 s+=2; // s=s+2;
 console.log(s);
-var t=8;
-t-=2; // t=t+2;
+let t=8;
+t-=2; // t=t-2;
 console.log(t);
 
 // Comparison operators
 
-var x=3;
-var y=3;
+let x=3;
+let y=3;
 if(x==y){
 	document.write("sum is : " );
 	document.write(x + y);
